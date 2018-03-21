9b757e30186b29853ba85ac48170700cccb4ce26
alex moore <amoore16@gmail.com>
2018-03-21 12:11:02 -0600

    initial commit

---
+++ assets/js/quotes.js
@@ -0,0 +1,19 @@
+$('#generate').on('click', function(){
+    $.ajax({
+        type:"GET",
+        url: "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies&count=1",
+        dataType: 'json',
+        beforeSend: function(xhr){
+            xhr.setRequestHeader("X-Mashape-key", "4POGpDKM7Nmshf1ADps8nLbZrlQ8p17EHTOjsnLYg6dbNIJoYA");
+            xhr
+        },
+        success: function(data){
+            $("#quote").text(data.quote);
+            $("#author").text(" - " + data.author);
+        },
+        error: function(){
+            console.log("error");
+        }
+    });
+});
+
