a9b3e3ba43d9ddcc9ae0327a8de0e2f2bbeacaa1
alex moore <amoore16@gmail.com>
2018-03-21 13:11:59 -0600

    styling

--- assets/js/quotes.js
+++ assets/js/quotes.js
@@ -6,13 +6,30 @@ $('#generate').on('click', function(){
         beforeSend: function(xhr){
             xhr.setRequestHeader("X-Mashape-key", "4POGpDKM7Nmshf1ADps8nLbZrlQ8p17EHTOjsnLYg6dbNIJoYA");
         },
+        complete: function(){
+            $("body").css("background-color", randomColor);
+        },
         success: function(data){
-            $("#quote").text(data.quote);
-            $("#author").text(" - " + data.author);
+            $("#quote").fadeOut(500, function(){
+                $("#quote").text(data.quote).hide().fadeIn(600);
+            });
+            $("#author").fadeOut(500, function(){
+                $("#author").text(" - " + data.author).hide().fadeIn(700);
+            });  
         },
         error: function(){
             console.log("error");
         }
     });
+
 });
 
+
+
+function randomColor() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+}
