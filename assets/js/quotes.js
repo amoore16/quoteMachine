7ff35e6b4f47da72d7ee2cf167f6521291c5a2e5
alex moore <amoore16@gmail.com>
2018-03-21 12:17:05 -0600

    forgot coffee

--- assets/js/quotes.js
+++ assets/js/quotes.js
@@ -5,7 +5,6 @@ $('#generate').on('click', function(){
         dataType: 'json',
         beforeSend: function(xhr){
             xhr.setRequestHeader("X-Mashape-key", "4POGpDKM7Nmshf1ADps8nLbZrlQ8p17EHTOjsnLYg6dbNIJoYA");
-            xhr
         },
         success: function(data){
             $("#quote").text(data.quote);
